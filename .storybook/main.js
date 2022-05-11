fd58f2be1f8ca734fb2374bf82031aa76ba10058
kilbot <paul@kilbot.com.au>
2022-05-11 13:24:26 +0100

    fix storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,52 +1,34 @@
 const path = require('path');
+
 const webpack = require('webpack');
 
 module.exports = {
-	core: {
-    builder: 'webpack5',
-	},
-	
-	stories: ['../src/**/*.stories.tsx'],
-
-	addons: [
-		'@storybook/preset-create-react-app',
-		'@storybook/addon-essentials',
-		'@storybook/addon-actions',
-		{
-      name: '@storybook/addon-react-native-web',
-      options: {
-				modulesToTranspile: [
-					'@wcpos/hooks',
-					'@wcpos/themes',
-					'@wcpos/utils',
-					'react-native-reanimated',
-					'react-native-gesture-handler'
-				],
-        babelPlugins: ['react-native-reanimated/plugin'],
-      },
-    },
-	],
-
-	framework: '@storybook/react',
-
-	typescript: {
-		// @TODO remove this https://github.com/styleguidist/react-docgen-typescript/issues/356
-		// reactDocgen: 'none',
-	},
-
-	babel: async (options) => ({
-    // Update your babel configuration here
+  stories: ['../src/**/*.stories.tsx'],
+  addons: ['@storybook/preset-create-react-app', '@storybook/addon-essentials', '@storybook/addon-actions', {
+    name: '@storybook/addon-react-native-web',
+    options: {
+      modulesToTranspile: ['@wcpos/hooks', '@wcpos/themes', '@wcpos/utils', 'react-native-reanimated', 'react-native-gesture-handler'],
+      babelPlugins: ['react-native-reanimated/plugin']
+    }
+  }],
+  framework: '@storybook/react',
+  typescript: {// @TODO remove this https://github.com/styleguidist/react-docgen-typescript/issues/356
+    // reactDocgen: 'none',
+  },
+  babel: async options => ({ // Update your babel configuration here
     ...options,
-		presets: ['@wcpos/babel-preset-expo']
+    presets: ['@wcpos/babel-preset-expo']
   }),
-
-	webpackFinal: async (config, { configType }) => {
-		config.resolve.alias = {
-			...(config.resolve.alias || {}),
-			// Mock expo-haptics
-			'expo-haptics$': path.resolve(__dirname, 'utils/expo-haptics'),
-		}
-			 
+  webpackFinal: async (config, {
+    configType
+  }) => {
+    config.resolve.alias = { ...(config.resolve.alias || {}),
+      // Mock expo-haptics
+      'expo-haptics$': path.resolve(__dirname, 'utils/expo-haptics')
+    };
     return config;
-	},
+  },
+  core: {
+    builder: 'webpack5'
+  }
 };
