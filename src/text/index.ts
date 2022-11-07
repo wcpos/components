abac9a823b478c0479a12744207198caf4f4f875
kilbot <paul@kilbot.com.au>
2022-11-07 18:29:53 +0100

    export TextProps

--- src/text/index.ts
+++ src/text/index.ts
@@ -1,4 +1,6 @@
-import { Text } from './text';
+import { Text, TextProps } from './text';
 import { TextSkeleton } from './skeleton';
+
+export type { TextProps };
 
 export default Object.assign(Text, { Skeleton: TextSkeleton });
