b9b2717146cfd30f252207623835d0c7b1ba0d2f
Paul Kilmurray <paul@kilbot.com>
2023-02-01 22:20:51 +0100

    forwardRef for TextInput with label

--- src/textinput/with-label.tsx
+++ src/textinput/with-label.tsx
@@ -1,37 +1,40 @@
 import * as React from 'react';
 import { TextInput as RNTextInput } from 'react-native';
 
+import useMergedRef from '@wcpos/hooks/src/use-merged-ref';
+
 import { TextInput, TextInputProps } from './textinput';
 import { InputWithLabel, InputWithLabelProps } from '../form-layout';
 
-export type TextInputWithLabelProps = TextInputProps & InputWithLabelProps;
+export type TextInputWithLabelProps = TextInputProps & Omit<InputWithLabelProps, 'children'>;
 
-export const TextInputWithLabel = ({
-	label,
-	helpText,
-	error,
-	onLabelPress,
-	...props
-}: TextInputWithLabelProps) => {
-	/**
-	 *
-	 */
-	const ref = React.useRef<RNTextInput>(null);
+export const TextInputWithLabel = React.forwardRef<RNTextInput, TextInputWithLabelProps>(
+	({ label, helpText, error, onLabelPress, ...props }, ref) => {
+		/**
+		 *
+		 */
+		const mergedRef = useMergedRef(React.useRef<RNTextInput>(null), ref);
 
-	/**
-	 * Focus the input on label press.
-	 */
-	const handleLabelPress = React.useCallback(() => {
-		ref.current?.focus();
-		onLabelPress && onLabelPress();
-	}, [onLabelPress]);
+		/**
+		 * Focus the input on label press.
+		 */
+		const handleLabelPress = React.useCallback(() => {
+			mergedRef.current?.focus();
+			onLabelPress && onLabelPress();
+		}, [mergedRef, onLabelPress]);
 
-	/**
-	 *
-	 */
-	return (
-		<InputWithLabel label={label} helpText={helpText} error={error} onLabelPress={handleLabelPress}>
-			<TextInput {...props} ref={ref} />
-		</InputWithLabel>
-	);
-};
+		/**
+		 *
+		 */
+		return (
+			<InputWithLabel
+				label={label}
+				helpText={helpText}
+				error={error}
+				onLabelPress={handleLabelPress}
+			>
+				<TextInput {...props} ref={mergedRef} />
+			</InputWithLabel>
+		);
+	}
+);
