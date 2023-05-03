27f9959701b2b380a6aef3f13249579cfa74b37d
kilbot <kilbot@users.noreply.github.com>
2023-05-03 18:24:13 +0000

    deploy: 6a7682930946aca7d579dd065b87b379195bc156

---
+++ 13795.bd9e9333.iframe.bundle.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_wcpos_components=self.webpackChunk_wcpos_components||[]).push([[13795],{"./src/icon/svg/fontawesome/solid/right-left.svg":(module,__unused_webpack_exports,__webpack_require__)=>{module.exports=__webpack_require__.p+"6f5ff7b2df071dc76678.svg"}}]);
