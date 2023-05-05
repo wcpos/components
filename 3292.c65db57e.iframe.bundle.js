24dd911575d9f31d3ce170a44f53c150db96468f
kilbot <kilbot@users.noreply.github.com>
2023-05-05 12:46:04 +0000

    deploy: e5b51bd256048341eb69304d1c367072f98a0ff4

---
+++ 3292.c65db57e.iframe.bundle.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_wcpos_components=self.webpackChunk_wcpos_components||[]).push([[3292],{"./src/icon/svg/fontawesome/solid/note-sticky.svg":(module,__unused_webpack_exports,__webpack_require__)=>{module.exports=__webpack_require__.p+"00da126d4f0aac0c7f32.svg"}}]);
---
+++ sb-addons/storybook-7/manager-bundle.js
@@ -0,0 +1,4 @@
+try{
+var m=__STORYBOOKADDONS__,{addons:r,types:_,mockChannel:O}=__STORYBOOKADDONS__;var S=__STORYBOOKTHEMING__,{CacheProvider:f,ClassNames:g,Global:P,ThemeProvider:C,background:N,color:w,convert:x,create:s,createCache:y,createGlobal:R,createReset:k,css:v,darken:B,ensure:D,ignoreSsrWarning:G,isPropValid:K,jsx:Y,keyframes:A,lighten:E,styled:H,themes:I,typography:M,useTheme:W,withTheme:j}=__STORYBOOKTHEMING__;var a=s({base:"light",brandTitle:"WCPOS",brandUrl:"https://wcpos.com"});r.setConfig({isFullscreen:!1,showNav:!0,showPanel:!0,panelPosition:"bottom",sidebarAnimations:!0,enableShortcuts:!0,isToolshown:!0,theme:a,selectedPanel:void 0});
+}catch(e){ console.error("[Storybook] One of your manager-entries failed: " + import.meta.url, e); }
+//# sourceMappingURL=manager-bundle.js.map
