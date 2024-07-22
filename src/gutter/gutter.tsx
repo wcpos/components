87ec257da0296639f2463f709a114ffaca8d373b
Paul Kilmurray <paul@kilbot.com>
2024-07-22 22:57:01 +0100

    update gutter component

--- src/gutter/gutter.tsx
+++ src/gutter/gutter.tsx
@@ -1,23 +1,44 @@
 import * as React from 'react';
 import { Pressable } from 'react-native';
+
 import * as Styled from './styles';
 import Icon from '../icon';
 
 export interface GutterProps {
 	children?: React.ReactNode;
 	style?: any;
+	direction?: 'horizontal' | 'vertical';
 }
 
-export const Gutter = ({ children, style }: GutterProps) => {
+export const Gutter = ({ children, style, direction = 'vertical' }: GutterProps) => {
 	return (
-		<Pressable style={[{ flexDirection: 'row', height: '100%' }, style]}>
+		<Pressable
+			style={[
+				{
+					flexDirection: direction === 'vertical' ? 'row' : 'column',
+					height: direction === 'vertical' ? '100%' : undefined,
+					width: direction === 'horizontal' ? '100%' : undefined,
+				},
+				style,
+			]}
+		>
 			{({
 				// @ts-ignore
 				hovered,
 			}) => (
-				<Styled.View hovered={hovered}>
+				<Styled.View
+					hovered={hovered}
+					style={{
+						height: direction === 'vertical' ? '100%' : '12px',
+						width: direction === 'horizontal' ? '100%' : '12px',
+					}}
+				>
 					{children || (
-						<Icon name="gripLinesVertical" size="small" type={hovered ? 'primary' : 'secondary'} />
+						<Icon
+							name={direction === 'horizontal' ? 'gripLines' : 'gripLinesVertical'}
+							size="xSmall"
+							type={hovered ? 'primary' : 'secondary'}
+						/>
 					)}
 				</Styled.View>
 			)}
--- src/gutter/styles.ts
+++ src/gutter/styles.ts
@@ -3,11 +3,9 @@ import styled, { css } from 'styled-components/native';
 import Platform from '@wcpos/utils/src/platform';
 
 export const View = styled.View<{ hovered: boolean }>`
-	height: 100%;
 	align-items: center;
 	justify-content: center;
 	background-color: ${({ hovered }) => (hovered ? 'rgba(0,0,0,0.05)' : 'transparent')};
-	width: 10px;
 
 	${Platform.OS !== 'ios' &&
 	Platform.OS !== 'android' &&
